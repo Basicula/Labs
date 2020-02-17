c57266577e9647717073ac4fe7c9a298ab71d050
basicula <maksim.basum@gmail.com>
2020-02-17 20:38:43 +0200

    Add comparison for knn classifier

--- 4thCourseS2/ML/knn_classifier.py
+++ 4thCourseS2/ML/knn_classifier.py
@@ -3,6 +3,8 @@ from classifier import Classifier
 import numpy as np
 import matplotlib.pyplot as plt
 
+from sklearn.neighbors import KNeighborsClassifier
+
 def euler_dist(x, y):
     return np.linalg.norm(x-y)
 
@@ -25,7 +27,7 @@ class KNNClassifier(Classifier):
         pass
 
 def test():
-    def generate_data(cluster_size, clusters_number):
+    def generate_group_data(cluster_size, clusters_number):
         x = np.zeros((cluster_size * clusters_number,2))
         y = np.zeros(cluster_size*clusters_number)
         index = 0
@@ -38,10 +40,19 @@ def test():
                 index+=1
         return x,y
         
-    data, labels = generate_data(100,5)
+    def generate_random_data(cluster_size, clusters_number):
+        x = np.zeros((cluster_size * clusters_number,2))
+        y = np.zeros(cluster_size*clusters_number)
+        for i in range(clusters_number * cluster_size):
+            x[i] = np.random.rand(2)
+            y[i] = np.random.randint(0,clusters_number)
+        return x,y
+        
+    data, labels = generate_group_data(100,5)
+    data, labels = generate_random_data(100,5)
     
     fig = plt.figure(constrained_layout=True)
-    gs = fig.add_gridspec(2,3)
+    gs = fig.add_gridspec(2,4)
     
     data_distr = fig.add_subplot(gs[0,0])
     data_distr.title.set_text("Data distribution")
@@ -54,7 +65,7 @@ def test():
     N = 2000
     
     xs = np.random.rand(N,2)
-    for i in range(5):
+    for i in range(3):
         cs = np.zeros(N)
         
         k = (i+1)*2
@@ -62,10 +73,18 @@ def test():
         for j in range(N):
             cs[j] = knn.predict(xs[j], k)
             
-        res = fig.add_subplot(gs[(i+1)//3,(i+1)%3])
+        res = fig.add_subplot(gs[0,i+1])
         res.title.set_text("Predictions with k = " + str(k))
         res.scatter(*xs.T, c = cs)
-    
+        
+        sk_knn =  KNeighborsClassifier(n_neighbors=k)
+        sk_knn.fit(data, labels)
+        
+        sk_res = fig.add_subplot(gs[1,i+1])
+        sk_res.title.set_text("sklearn predictions with k = " + str(k))
+        cs = sk_knn.predict(xs)
+        sk_res.scatter(*xs.T, c = cs)
+        
     plt.show()
 
 if __name__ == "__main__":
