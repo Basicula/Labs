a72e37938364b75ded33a328879e708ae46331db
basicula <maksim.basum@gmail.com>
2020-04-25 16:17:27 +0300

    Update task

--- 4thCourseS2/Parallel/ParcsJAVA/FuncMapMatrix/src/Map.java
+++ 4thCourseS2/Parallel/ParcsJAVA/FuncMapMatrix/src/Map.java
@@ -1,12 +1,32 @@
 import parcs.*;
 
 public class Map implements AM{
+    static int gcd(int a, int b) 
+      { 
+          if (a == 0) 
+              return b; 
+          return gcd(b % a, a); 
+      } 
+    
+    static int phi(int n) 
+      { 
+        int result = 1; 
+        for (int i = 2; i < n; i++) 
+            if (gcd(i, n) == 1) 
+                result++; 
+        return result; 
+      } 
+  
     public void run(AMInfo info){
-        int x,y,r;
-        x = info.parent.readInt();
-        y = info.parent.readInt();
-        System.out.println(x + '+' + y);
-        r = x+y;
-        info.parent.write(r);
+        int n,m;
+        n = info.parent.readInt();
+        m = info.parent.readInt();
+        int[][] temp = new int[n][m];
+        for (int i = 0; i < n; ++i)
+          for (int j = 0; j < m; ++j)
+            temp[i][j] = info.parent.readInt();
+        for (int i = 0; i < n; ++i)
+          for (int j = 0; j < m; ++j)
+            info.parent.write(phi(temp[i][j]));
     }
 }
--- 4thCourseS2/Parallel/ParcsJAVA/FuncMapMatrix/src/Matrix.java
+++ 4thCourseS2/Parallel/ParcsJAVA/FuncMapMatrix/src/Matrix.java
@@ -3,17 +3,6 @@ import java.io.*;
 import parcs.*;
 
 public class Matrix implements AM {
-
-    public class Position 
-      {
-      public int x;
-      public int y;
-      public Position(int _x, int _y)
-        {
-        x = _x;
-        y = _y;
-        }
-      }
     
     public static void main(String[] args) {
         task curtask = new task();
@@ -32,36 +21,54 @@ public class Matrix implements AM {
       n = sc.nextInt();
       m = sc.nextInt();
       System.out.println("Size: " + n + "x" + m);
-      int[][] res = new int[n][m];
-      int workers_cnt = 2;
-      Queue<point> points = new LinkedList<point>();
-      Queue<channel> channels = new LinkedList<channel>();
-      Queue<Position> positions = new LinkedList<Position>();
+      
+      int[][] matrix = new int[n][m];
       for (int i = 0; i < n; ++i)
         for (int j = 0; j < m; ++j)
+          matrix[i][j] = sc.nextInt();
+      
+      int[][] res = new int[n][m];
+      
+      int workers_cnt = 2;
+      point[] points = new point[workers_cnt];
+      channel[] channels = new channel[workers_cnt];
+      for (int i = 0; i < workers_cnt; ++i)
+        {
+        points[i] = info.createPoint();
+        channels[i] = points[i].createChannel();
+        channels[i].write(n / workers_cnt);
+        channels[i].write(m);
+        }
+      
+      for (int i = 0; i < n / workers_cnt; ++i)
+        for (int j = 0; j < m; ++j)
           {
-          point p = info.createPoint();
-          channel c = p.createChannel();
-          p.execute("Map");
-          c.write(i);
-          c.write(j);
-          points.add(p);
-          channels.add(c);
-          positions.add(new Position(i,j));
-          if(points.size() == workers_cnt)
+          for (int k = 0; k < workers_cnt; ++k)
             {
-            while(points.size() > 0)
-              {
-              Position pos = positions.remove();
-              channel ch = channels.remove();
-              res[pos.x][pos.y] = ch.readInt();
-              points.remove();
-              }
+            point p = points[k];
+            channel c = channels[k];
+            p.execute("Map");
+            c.write(matrix[i + k * n / workers_cnt][j]);
+            }
+          }
+      for (int i = 0; i < n / workers_cnt; ++i)
+        for (int j = 0; j < m; ++j)
+          {
+          for (int k = 0; k < workers_cnt; ++k)
+            {
+            point p = points[k];
+            channel c = channels[k];
+            res[i + k * n / workers_cnt][j] = c.readInt();
             }
           }
       try{
           PrintWriter out = new PrintWriter(new FileWriter(info.curtask.addPath("Matrix.res")));
-          out.println(res);
+          for (int i = 0; i < n; ++i)
+            {
+            for (int j = 0; j < m; ++j)
+              out.print(res[i][j] + " ");
+            out.println();
+            }
           out.close();
       } catch (IOException e) {e.printStackTrace(); return;}
     }
