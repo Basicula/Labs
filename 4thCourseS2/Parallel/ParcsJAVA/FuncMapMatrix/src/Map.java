006acee9eb697cfb3fc34f95ac3a00742cee7b9b
basicula <maksim.basum@gmail.com>
2020-04-25 13:58:45 +0300

    Test dump

--- 4thCourseS2/Parallel/ParcsJAVA/FuncMapMatrix/src/Map.java
+++ 4thCourseS2/Parallel/ParcsJAVA/FuncMapMatrix/src/Map.java
@@ -4,7 +4,7 @@ public class Map implements AM{
     public void run(AMInfo info){
         int x,y,r;
         x = info.parent.readInt();
-        System.out.print(x);
+        System.out.println(x);
         //y = info.parent.readInt();
         //System.out.print('+');
         //System.out.print(y);
